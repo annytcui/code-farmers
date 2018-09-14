645c901b72e910fcedb41fc56e66f3175b4ecf92
tianyuancuipan <tianyuan.cui.sc@gmail.com>
2018-09-14 15:02:49 -0500

    Add user login test

---
+++ test/integration/users_login_test.rb
@@ -0,0 +1,16 @@
+require 'test_helper'
+
+class UsersLoginTest < ActionDispatch::IntegrationTest
+
+  test "login with invalid information" do
+    get login_path
+    assert_template 'sessions/new'
+    post login_path, params: { session: { email: "", password: "" } }
+    assert_template 'sessions/new'
+    assert_not flash.empty?
+    assert_select 'div.alert-danger'
+    get root_path
+    assert flash.empty?
+    assert_select 'div.alert-danger', false
+  end
+end
