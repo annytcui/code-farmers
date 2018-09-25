b7ace76eb93010ad4407164e0843f75193f7b1ef
tianyuancuipan <tianyuan.cui.sc@gmail.com>
2018-09-25 15:27:40 -0500

    Fix typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
   host = 'shrouded-plains-30303.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
-    :address => 'stmp.sendgrid.net',
+    :address => 'smtp.sendgrid.net',
     :port => '587',
     :authentication => :plain,
     :user_name => ENV['SENDGRID_USERNAME'],
