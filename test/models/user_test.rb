54fdee07078914c72ed8683cb7531ac2aea4b10b
tianyuancuipan <tianyuan.cui.sc@gmail.com>
2018-10-01 12:41:00 -0500

    Add test for followers

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -93,6 +93,7 @@ class UserTest < ActiveSupport::TestCase
     assert_not anny.following?(mark)
     anny.follow(mark)
     assert anny.following?(mark)
+    assert mark.followers.include?(anny)
     anny.unfollow(mark)
     assert_not anny.following?(mark)
   end
